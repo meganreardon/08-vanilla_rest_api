a55667211c998cb58cd3ecb8f68941ff9760a3de
Megan Reardon <meganr@gmail.com>
2016-12-17 20:56:51 -0800

    after refactoring for response module

---
+++ lab-megan/route/cheese-route.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const storage = require('../lib/storage.js');
+const Cheese = require('../model/cheese.js');
+const response = require('../lib/response.js');
+
+module.exports = function(router) {
+
+  router.get('/api/cheese', function(req, res) {
+    // console.log(':::reached inside get api function:::');
+    if (req.url.query.id) {
+      storage.fetchItem('cheese', req.url.query.id)
+      .then( cheese => {
+        response.sendJSON(res, 200, cheese);
+        // res.writeHead(200, {'Content-Type': 'application/json'});
+        // res.write(JSON.stringify(cheese));
+        // res.end();
+      })
+      .catch( err => {
+        // console.log('::: reached get function error block of server.js :::');
+        console.error(err);
+        response.sendText(res, 404, 'File not found.');
+        // res.writeHead(404, {'Content-Type': 'text/plain'});
+        // res.write('File not found. Who moved my cheese?');
+        // res.end();
+      });
+      return;
+    }
+    response.sendText(res, 400, 'Bad request.');
+    // res.writeHead(400, {'Content-Type': 'text/plain'});
+    // res.write('Bad request.');
+    // res.end();
+  });
+
+  router.delete('/api/cheese', function(req, res) {
+    // console.log('::: reached inside delete block of server.js :::');
+    if (req.url.query.id) {
+      storage.deleteItem('cheese', req.url.query.id)
+      .then( () => {
+        response.sendText(res, 204, null); // TODO is null what is best to use here?
+        // res.writeHead(204, {'Content-Type': 'text/plain'});
+        // res.end();
+      })
+      .catch( err => {
+        console.error(err);
+        response.sendText(res, 404, 'File not found.');
+        // res.writeHead(404, {'Content-Type': 'text/plain'});
+        // res.write('File not found. Who moved my cheese?');
+        // res.end();
+      });
+      return;
+    }
+    response.sendText(res, 400, 'Bad request.');
+    // res.writeHead(400, {'Content-Type': 'text/plain'});
+    // res.write('Bad request.');
+    // res.end();
+  });
+
+  router.post('/api/cheese', function(req, res) {
+    try {
+      var cheese = new Cheese(req.body.color, req.body.pokableness);
+      storage.createItem('cheese', cheese);
+      response.sentJSON(res, 200, cheese);
+      // res.writeHead(200,{'Content-Type': 'application/json'});
+      // res.write(JSON.stringify(cheese));
+      // res.end();
+    } catch (err) {
+      console.error(err);
+      response.sentText(res, 400, 'Bad request.');
+      // res.writeHead(400,{'Content-Type': 'text/plain'});
+      // res.write('Bad request.');
+      // res.end();
+    }
+  });
+
+};
--- lab-megan/server.js
+++ lab-megan/server.js
@@ -1,71 +1,71 @@
 'use strict';
 
 const http = require('http');
-const Cheese = require('./model/cheese.js');
 const Router = require('./lib/router.js');
-const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
-router.get('/api/cheese', function(req, res) {
-  // console.log(':::reached inside get api function:::');
-  if (req.url.query.id) {
-    storage.fetchItem('cheese', req.url.query.id)
-    .then( cheese => {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.write(JSON.stringify(cheese));
-      res.end();
-    })
-    .catch( err => {
-      // console.log('::: reached get function error block of server.js :::');
-      console.error(err);
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write('File not found. Who moved my cheese?');
-      res.end();
-    });
-    return;
-  }
-  res.writeHead(400, {'Content-Type': 'text/plain'});
-  res.write('Bad request.');
-  res.end();
-});
+require('./route/cheese-route.js')(router);
 
-// FIRST TRY
-router.delete('/api/cheese', function(req, res) {
-  // console.log('::: reached inside delete block of server.js :::');
-  if (req.url.query.id) {
-    storage.deleteItem('cheese', req.url.query.id)
-    .then( () => {
-      res.writeHead(204, {'Content-Type': 'text/plain'});
-      res.end();
-    })
-    .catch( err => {
-      console.error(err);
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write('File not found. Who moved my cheese?');
-      res.end();
-    });
-    return;
-  }
-  res.writeHead(400, {'Content-Type': 'text/plain'});
-  res.write('Bad request.');
-  res.end();
-});
-
-router.post('/api/cheese', function(req, res) {
-  try {
-    var cheese = new Cheese(req.body.color, req.body.pokableness);
-    storage.createItem('cheese', cheese);
-    res.writeHead(200,{'Content-Type': 'application/json'});
-    res.write(JSON.stringify(cheese));
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(400,{'Content-Type': 'text/plain'});
-    res.write('Bad request.');
-    res.end();
-  }
-});
+// router.get('/api/cheese', function(req, res) {
+//   // console.log(':::reached inside get api function:::');
+//   if (req.url.query.id) {
+//     storage.fetchItem('cheese', req.url.query.id)
+//     .then( cheese => {
+//       res.writeHead(200, {'Content-Type': 'application/json'});
+//       res.write(JSON.stringify(cheese));
+//       res.end();
+//     })
+//     .catch( err => {
+//       // console.log('::: reached get function error block of server.js :::');
+//       console.error(err);
+//       res.writeHead(404, {'Content-Type': 'text/plain'});
+//       res.write('File not found. Who moved my cheese?');
+//       res.end();
+//     });
+//     return;
+//   }
+//   res.writeHead(400, {'Content-Type': 'text/plain'});
+//   res.write('Bad request.');
+//   res.end();
+// });
+//
+// // FIRST TRY
+// router.delete('/api/cheese', function(req, res) {
+//   // console.log('::: reached inside delete block of server.js :::');
+//   if (req.url.query.id) {
+//     storage.deleteItem('cheese', req.url.query.id)
+//     .then( () => {
+//       res.writeHead(204, {'Content-Type': 'text/plain'});
+//       res.end();
+//     })
+//     .catch( err => {
+//       console.error(err);
+//       res.writeHead(404, {'Content-Type': 'text/plain'});
+//       res.write('File not found. Who moved my cheese?');
+//       res.end();
+//     });
+//     return;
+//   }
+//   res.writeHead(400, {'Content-Type': 'text/plain'});
+//   res.write('Bad request.');
+//   res.end();
+// });
+//
+// router.post('/api/cheese', function(req, res) {
+//   try {
+//     var cheese = new Cheese(req.body.color, req.body.pokableness);
+//     storage.createItem('cheese', cheese);
+//     res.writeHead(200,{'Content-Type': 'application/json'});
+//     res.write(JSON.stringify(cheese));
+//     res.end();
+//   } catch (err) {
+//     console.error(err);
+//     res.writeHead(400,{'Content-Type': 'text/plain'});
+//     res.write('Bad request.');
+//     res.end();
+//   }
+// });
 
 const server = http.createServer(router.route());
 
